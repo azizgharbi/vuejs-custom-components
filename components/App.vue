15cc5a8af9e214a2d954e7fe1ffba52fc48d1e78
Aziz Gharbi <ag@brainfinance.com>
2019-09-16 11:07:28 -0400

    styling

--- components/App.vue
+++ components/App.vue
@@ -2,7 +2,12 @@
   <div>
     <div class="demo">
       <button v-on:click="open">open</button>
-      <Modal :active="active" :closeable="closeable" :closeOnOutsideClick="closeOnOutsideClick">
+      <Modal
+        :active="active"
+        :closeable="closeable"
+        :closeOnOutsideClick="closeOnOutsideClick"
+        :state="state"
+      >
         <div slot="header">header</div>
         <p>hello</p>
         <i class="fa fa-twitter"></i>
@@ -12,11 +17,19 @@
     </div>
 
     <div class="options">
-      <h1>Options</h1>
-      <input type="checkbox" id="closeable" v-model="closeable">
-      <label for="closeable">closeable : {{ closeable }}</label>
-      <input type="checkbox" id="closeOnOutsideClick" v-model="closeOnOutsideClick">
-      <label for="closeOnOutsideClick">closeOnOutsideClick : {{ closeOnOutsideClick }}</label>
+      <h1>Options:</h1>
+      <input type="checkbox" id="closeable" v-model="closeable" />
+      <label for="closeable">Closeable : {{ closeable }}</label>
+      <input type="checkbox" id="closeOnOutsideClick" v-model="closeOnOutsideClick" />
+      <label for="closeOnOutsideClick">Close On Outside Click : {{ closeOnOutsideClick }}</label>
+      <label for="state">State</label>
+      <select v-model="state" id="state">
+        <option value="normal">normal</option>
+        <option value="success">success</option>
+        <option value="info">info</option>
+        <option value="alert">alert</option>
+        <option value="danger">danger</option>
+      </select>
     </div>
   </div>
 </template>
